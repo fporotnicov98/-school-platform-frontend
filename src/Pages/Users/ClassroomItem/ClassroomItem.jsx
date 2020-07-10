c35574525b13897dcb5a26ddde703cae37d28489
golovan8672 <golovan8672@gmail.com>
2020-07-10 15:19:40 +0500

    add

--- src/Pages/Users/ClassroomItem/ClassroomItem.jsx
+++ src/Pages/Users/ClassroomItem/ClassroomItem.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {compose} from "redux";
-import {Redirect, withRouter} from "react-router-dom";
-import {connect} from "react-redux";
-import {getClassroom} from "../../../Redux/classReducer";
-import {getStudent, getTeacher, updateStudent, updateTeacher} from "../../../Redux/userReducer";
+import React, { Component } from 'react';
+import { compose } from "redux";
+import { Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { getClassroom } from "../../../Redux/classReducer";
+import { getStudent, getTeacher, updateStudent, updateTeacher } from "../../../Redux/userReducer";
 import './ClassroomItem.scss'
 import M from "materialize-css";
 
@@ -24,7 +24,7 @@ class ClassroomItem extends Component {
     }
 
     handleStudents = () => {
-        this.setState({isClick: !this.state.isClick})
+        this.setState({ isClick: !this.state.isClick })
     }
 
     render() {
@@ -37,9 +37,9 @@ class ClassroomItem extends Component {
                             <span className="card-title">{this.props.classNumber}</span>
                             <span className='class-teacher'>Классный руководитель:
                                     <a className="btn-floating btn-small waves-effect waves-light cyan darken-2">
-                                        <i className="material-icons">add</i>
-                                    </a>
-                                </span>
+                                    <i className="material-icons">add</i>
+                                </a>
+                            </span>
                         </div>
                         <span>Учащиеся: </span>
                         {
@@ -53,6 +53,7 @@ class ClassroomItem extends Component {
                                             <div className='login'>{item.login}</div>
                                             <div className='email'>{item.email}</div>
                                             <div className='mobileNumber'>{item.mobileNumber}</div>
+                                            <a onClick={() => this.props.updateStudent(item._id, item.fio, item.login, item.email, item.mobileNumber, "none")} href="#s"><i className='material-icons'>remove_circle_outline</i></a>
                                         </div>
                                     </div>
                                     : null
@@ -60,31 +61,29 @@ class ClassroomItem extends Component {
                         }
                         <span className='add-student'>Добавить ученика:
                             <a onClick={() => this.handleStudents()}
-                               className="btn-floating btn-small waves-effect waves-light cyan darken-2">
+                                className="btn-floating btn-small waves-effect waves-light cyan darken-2">
                                 <i className="material-icons">add</i>
                             </a>
                         </span>
                         <div className='students'>
                             {this.state.isClick &&
-                            this.props.students.map((item, index) =>
-                                <div className='items white z-depth-1-half'>
-                                    {item.classroom !== this.props.classId
+                                this.props.students.map((item, index) => 
+                                    item.classroom === 'none'
                                         ?
-                                        <div className='info'>
-                                            <span>{index + 1}.</span>
-                                            <div className='fio'>{item.fio}</div>
-                                            <div className='login'>{item.login}</div>
-                                            <div className='email'>{item.email}</div>
-                                            <div className='mobileNumber'>{item.mobileNumber}</div>
-                                            <a onClick={() => this.props.updateStudent(item._id, item.fio, item.login, item.email, item.mobileNumber, this.props.classId)} href="#s"><i className='material-icons'>add_circle_outline</i></a>
+                                        <div className='items white z-depth-1-half'>
+                                            <div className='info'>
+                                                <span>{index + 1}.</span>
+                                                <div className='fio'>{item.fio}</div>
+                                                <div className='login'>{item.login}</div>
+                                                <div className='email'>{item.email}</div>
+                                                <div className='mobileNumber'>{item.mobileNumber}</div>
+                                                <a onClick={() => this.props.updateStudent(item._id, item.fio, item.login, item.email, item.mobileNumber, this.props.classId)} href="#s"><i className='material-icons'>add_circle_outline</i></a>
+                                            </div>
                                         </div>
                                         : null
-                                    }
-                                </div>
-                            )
+                                )
                             }
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -103,5 +102,5 @@ const mapStateToProps = state => {
 
 export default compose(
     withRouter,
-    connect(mapStateToProps, {getClassroom, getTeacher, getStudent, updateTeacher, updateStudent})
+    connect(mapStateToProps, { getClassroom, getTeacher, getStudent, updateTeacher, updateStudent })
 )(ClassroomItem);
