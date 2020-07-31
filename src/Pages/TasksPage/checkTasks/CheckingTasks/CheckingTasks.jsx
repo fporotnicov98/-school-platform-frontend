6cdaa294c1a1269f021f0f0a57e7d295b0abc2b7
Fedor <porotnikov.fiodor@yandex.ru>
2020-07-31 19:57:24 +0500

    'checkingTasks'

--- src/Pages/TasksPage/checkTasks/CheckingTasks/CheckingTasks.jsx
+++ src/Pages/TasksPage/checkTasks/CheckingTasks/CheckingTasks.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
+import React, {useState} from 'react';
+import {NavLink, Redirect} from "react-router-dom";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const CheckingTasks = (props) => {
+
+    let [rating, setRating] = useState('')
+    let [notation, setNotation] = useState('')
+
+    let handleRating = (e) => {
+        setRating(e.target.value)
+    }
+    let handleNotation = (e) => {
+        setNotation(e.target.value)
+    }
+
+    if (!props.auth.isAuth) return <Redirect to={'/'}/>
     return (
         <div className='wrapper'>
             <nav className='blue-grey lighten-4'>
@@ -36,10 +51,23 @@ const CheckingTasks = (props) => {
                         <span>{props.homeworkItem.answerToTask}</span>
                     </div>
                     <div className='description'>Примечание
-                        <span></span>
+                        <div className="input-field">
+                            <textarea className='materialize-textarea' value={notation}
+                                      onChange={handleNotation}/>
+                        </div>
                     </div>
                     <div className='answer'>Оценка
-                        
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={rating}
+                            onChange={handleRating}
+                        >
+                            <MenuItem value={'2'}>2</MenuItem>
+                            <MenuItem value={'3'}>3</MenuItem>
+                            <MenuItem value={'4'}>4</MenuItem>
+                            <MenuItem value={'5'}>5</MenuItem>
+                        </Select>
                     </div>
                     <button className="btn waves-effect waves-light cyan darken-2" type="submit" name="action">
                         Отправить результат првоерки
